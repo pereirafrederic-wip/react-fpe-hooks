569c2a1d46069cfb425705fccdb3e09b49cbf9c6
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-10-30 18:47:57 +0100

    Add files via upload

---
+++ HookEffect.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+export default function HookEffect() {
+  const [count, setCount] = useState(0);
+
+  // Similaire à componentDidMount et componentDidUpdate :
+  useEffect(() => {
+    // Met à jour le titre du document via l’API du navigateur
+    console.info("count", count)
+  });
+
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+    </div>
+  );
+}
---
+++ HookState.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function HookState() {
+  // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+    </div>
+  );
+}
